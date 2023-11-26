0ec3bf97eeab90c909da6d1453f4dff7a64b7097
Michael Chop <007micle007@gmail.com>
2023-11-26 19:25:42 +0300

    Finished with the authentication functionality

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -2,8 +2,14 @@
 
 import {Button} from "@/components/ui/button";
 import {ArrowRightIcon} from "lucide-react";
+import {useConvexAuth} from "convex/react";
+import {Spinner} from "@/components/spinner";
+import Link from "next/link";
+import {SignInButton} from "@clerk/clerk-react";
 
 export const Heading = () => {
+    const {isAuthenticated, isLoading} = useConvexAuth()
+
     return (
         <div className={"max-w-3xl space-y-4"}>
             <h1 className={"text-3xl sm:text-5xl md:text-6xl font-bold"}>
@@ -15,10 +21,29 @@ export const Heading = () => {
                 better, faster work happens
             </h3>
 
-            <Button>
-                Enter Memo
-                <ArrowRightIcon className={"h-4 w-4 ml-2"} />
-            </Button>
+            {isLoading && (
+                <div className={"w-full flex items-center justify-center"}>
+                    <Spinner size={"lg"} />
+                </div>
+            )}
+
+            {isAuthenticated && !isLoading && (
+                <Button asChild>
+                    <Link href={"/documents"}>
+                        Enter Memo
+                        <ArrowRightIcon className={"h-4 w-4 ml-2"} />
+                    </Link>
+                </Button>
+            )}
+
+            {!isAuthenticated && !isLoading && (
+                <SignInButton mode={"modal"}>
+                    <Button>
+                        Get Memo free
+                        <ArrowRightIcon className={"h-4 w-4 ml-2"} />
+                    </Button>
+                </SignInButton>
+            )}
         </div>
     );
 };
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+   providers: [
+       {
+           domain: "https://national-penguin-51.clerk.accounts.dev",
+           applicationID: "convex"
+       }
+   ]
+}
