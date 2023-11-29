197e52e3f9e92773b1292dc89e0a0630fbe61708
Michael Chop <007micle007@gmail.com>
2023-11-29 23:35:44 +0300

    Added a Banner component to the project

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -6,6 +6,8 @@ import {api} from "@/convex/_generated/api";
 import {Id} from "@/convex/_generated/dataModel";
 import {MenuIcon} from "lucide-react";
 import {Title} from "./title";
+import {Banner} from "./banner";
+import {Menu} from "./menu";
 
 interface INavbarProps {
     isCollapsed: boolean
@@ -20,8 +22,12 @@ export const Navbar = ({isCollapsed, onResetWidth}: INavbarProps) => {
 
     if(document === undefined) {
         return (
-            <nav className={"bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center"}>
+            <nav className={"bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between"}>
                 <Title.Skeleton />
+
+                <div className={"flex items-center gap-x-2"}>
+                    <Menu.Skeleton />
+                </div>
             </nav>
         )
     }
@@ -43,8 +49,15 @@ export const Navbar = ({isCollapsed, onResetWidth}: INavbarProps) => {
 
                 <div className={"flex items-center justify-between w-full"}>
                     <Title initialData={document} />
+
+                    <div className={"flex items-center gap-x-2"}>
+                        <Menu documentId={document._id} />
+                    </div>
                 </div>
             </nav>
+            {document.isArchived && (
+                <Banner documentId={document._id} />
+            )}
         </>
     )
 }
