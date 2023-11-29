852c6274a7a0db6610952f1cb3c9df10e12ae8ab
Michael Chop <007micle007@gmail.com>
2023-11-29 21:42:04 +0300

    Finished with the main Settings functionality

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -14,9 +14,11 @@ import {DocumentList} from "./document-list";
 import {TrashBox} from "./trash-box";
 import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
 import {useSearch} from "@/hooks/use-search";
+import {useSettings} from "@/hooks/use-settings";
 
 export const Navigation = () => {
     const search = useSearch()
+    const settings = useSettings()
     const pathname = usePathname()
     const isMobile = useMediaQuery("(max-width: 768px)")
     const create = useMutation(api.documents.create)
@@ -138,7 +140,7 @@ export const Navigation = () => {
                     <Item
                         label={"Settings"}
                         icon={Settings}
-                        onClick={() => {}}
+                        onClick={settings.onOpen}
                     />
                     <Item
                         onClick={handleCreate}
