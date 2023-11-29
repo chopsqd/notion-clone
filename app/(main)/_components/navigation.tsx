4357a7d5baccac9fb3dec849fb21d9f808f05a9f
Michael Chop <007micle007@gmail.com>
2023-11-29 22:26:17 +0300

    Finished with the main Navbar functionality

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -3,7 +3,7 @@
 import {ChevronsLeftIcon, MenuIcon, PlusCircle, Search, Settings, Trash} from "lucide-react";
 import {ElementRef, useEffect, useRef, useState} from "react";
 import {useMediaQuery} from "usehooks-ts";
-import {usePathname} from "next/navigation";
+import {useParams, usePathname} from "next/navigation";
 import {cn} from "@/lib/utils";
 import {UserItem} from "./user-item";
 import {useMutation} from "convex/react";
@@ -15,10 +15,12 @@ import {TrashBox} from "./trash-box";
 import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
 import {useSearch} from "@/hooks/use-search";
 import {useSettings} from "@/hooks/use-settings";
+import {Navbar} from "./navbar";
 
 export const Navigation = () => {
     const search = useSearch()
     const settings = useSettings()
+    const params = useParams()
     const pathname = usePathname()
     const isMobile = useMediaQuery("(max-width: 768px)")
     const create = useMutation(api.documents.create)
@@ -185,9 +187,16 @@ export const Navigation = () => {
                     isMobile && "left-0 w-full"
                 )}
             >
-                <nav className={"bg-transparent px-3 py-2 w-full"}>
-                    {isCollapsed && <MenuIcon onClick={resetWidth} role={"button"} className={"h-6 w-6 text-muted-foreground"} />}
-                </nav>
+                {!!params.documentId ? (
+                    <Navbar
+                        isCollapsed={isCollapsed}
+                        onResetWidth={resetWidth}
+                    />
+                ) : (
+                    <nav className={"bg-transparent px-3 py-2 w-full"}>
+                        {isCollapsed && <MenuIcon onClick={resetWidth} role={"button"} className={"h-6 w-6 text-muted-foreground"} />}
+                    </nav>
+                )}
             </div>
         </>
     )
