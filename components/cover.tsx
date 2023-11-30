7eaf7bfeccd03f60f45161781fef444ccce0c978
Michael Chop <007micle007@gmail.com>
2023-11-30 21:22:20 +0300

    Updated some file uploading methods

--- components/cover.tsx
+++ components/cover.tsx
@@ -10,6 +10,7 @@ import {api} from "@/convex/_generated/api";
 import {removeCoverImage} from "@/convex/documents";
 import {useParams} from "next/navigation";
 import {Id} from "@/convex/_generated/dataModel";
+import {useEdgeStore} from "@/lib/edgestore";
 
 interface ICoverProps {
     url?: string
@@ -17,11 +18,18 @@ interface ICoverProps {
 }
 
 export const Cover = ({url, preview}: ICoverProps) => {
+    const {edgestore} = useEdgeStore()
     const params = useParams()
     const coverImage = useCoverImage()
     const removeCoverImage = useMutation(api.documents.removeCoverImage)
 
-    const onRemove = () => {
+    const onRemove = async () => {
+        if(url) {
+            await edgestore.publicFiles.delete({
+                url
+            })
+        }
+
         removeCoverImage({
             id: params.documentId as Id<"documents">
         })
@@ -45,7 +53,7 @@ export const Cover = ({url, preview}: ICoverProps) => {
             {url && !preview && (
                 <div className={"opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2"}>
                     <Button
-                        onClick={coverImage.onOpen}
+                        onClick={() => coverImage.onReplace(url)}
                         className={"text-muted-foreground text-xs"}
                         variant={"outline"}
                         size={"sm"}
